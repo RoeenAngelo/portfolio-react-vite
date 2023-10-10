0ae0ab526d0af37b180c41d7e73f07d3e5594a0d
Roeen <roeenacal@gmail.com>
2023-10-10 16:33:05 -0600

    Add responsive Navbar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { useEffect } from "react";
 import { useState } from "react";
 import "./App.css";
 import useMediaQuery from "./hooks/useMediaQuery";
@@ -6,10 +7,35 @@ import Navbar from "./scenes/Navbar";
 function App() {
   const [selectedPage, setSelectedPage] = useState("home");
   const isAboveMediumScreens = useMediaQuery("(min-width: 1060px)");
+  const [isTopOfPage, setisTopOfPage] = useState(true);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollPos = window.scrollY;
+      const width = window.innerWidth;
+      if (scrollPos === 0) setisTopOfPage(true);
+      if (scrollPos !== 0) setisTopOfPage(false);
+
+      if (width > 960 && scrollPos < 600) {
+        setSelectedPage("home");
+      } else if (width > 1060 && scrollPos < 1300) {
+        setSelectedPage("skills");
+      } else if (width > 1060 && scrollPos < 2900) {
+        setSelectedPage("projects");
+      } else if (width > 1060 && scrollPos < 4600) {
+        setSelectedPage("contact");
+      }
+    };
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
 
   return (
     <div className="app bg-deep-blue md:w-full md:max-width-[1300px]">
-      <Navbar />
+      <Navbar
+        selectedPage={selectedPage}
+        setSelectedPage={setSelectedPage}
+      />
     </div>
   );
 }
--- src/scenes/Navbar.jsx
+++ src/scenes/Navbar.jsx
@@ -1,15 +1,149 @@
-import React from "react";
-import { useState } from "react";
+import { AnimatePresence, motion } from "framer-motion";
+import React, { useState } from "react";
+import AnchorLink from "react-anchor-link-smooth-scroll";
 import useMediaQuery from "../hooks/useMediaQuery";
 
-const Navbar = () => {
+import menuButton from "../assets/menu-icon.svg";
+import closeButton from "../assets/close-button-black.png";
+
+const Link = ({ page, selectedPage, setSelectedPage }) => {
+  const lowerCasePage = page.toLowerCase();
+  return (
+    <AnchorLink
+      className={`${selectedPage === lowerCasePage ? "text-yellow" : ""}
+    hover:text-yellow transition duration-500`}
+      href={`#${lowerCasePage}`}
+      onClick={() => setSelectedPage(lowerCasePage)}
+    >
+      {page}
+    </AnchorLink>
+  );
+};
+
+const Navbar = ({ isTopOfPage, selectedPage, setSelectedPage }) => {
   const [isMenuToggled, setIsMenuToggled] = useState(false);
   const isAboveSmallScreens = useMediaQuery("(min-width: 768px)");
+  const navbarBackground = isTopOfPage ? "" : "bg-blue";
 
   return (
-    <nav className="z-40 w-full fixed top-0 py-6">
+    <nav className={`${navbarBackground} z-40 w-full fixed top-0 py-6`}>
       <div className="flex items-center justify-between mx-auto w-5/6">
         <h4 className="font-playfair text-3xl font-bold">RA</h4>
+
+        {/* DESKTOP NAV */}
+        {isAboveSmallScreens ? (
+          <div className="flex justify-between gap-16 font-opensans text-sm font-semibold">
+            <Link
+              page="Home"
+              selectedPage={selectedPage}
+              setSelectedPage={setSelectedPage}
+            />
+            <Link
+              page="Skills"
+              selectedPage={selectedPage}
+              setSelectedPage={setSelectedPage}
+            />
+            <Link
+              page="Projects"
+              selectedPage={selectedPage}
+              setSelectedPage={setSelectedPage}
+            />
+            <Link
+              page="Contact"
+              selectedPage={selectedPage}
+              setSelectedPage={setSelectedPage}
+            />
+          </div>
+        ) : (
+          <button
+            className="rounded-full bg-blue p-2"
+            onClick={() => setIsMenuToggled(!isMenuToggled)}
+          >
+            <img
+              src={menuButton}
+              alt="menu-icon"
+            />
+          </button>
+        )}
+
+        {/* MOBILE NAV */}
+        <AnimatePresence>
+          {!isAboveSmallScreens && isMenuToggled && (
+            <motion.div
+              key="mobile-nav"
+              variants={{
+                hide: {
+                  x: "100%",
+                  transition: {
+                    type: "spring",
+                    bounce: 0.1,
+                    when: "afterChildren",
+                    staggerChildren: 0.25,
+                  },
+                },
+                show: {
+                  x: "0%",
+                  transition: {
+                    type: "spring",
+                    bounce: 0.1,
+                    when: "beforeChildren",
+                    staggerChildren: 0.25,
+                  },
+                },
+              }}
+              initial="hide"
+              animate="show"
+              exit="hide"
+              className="fixed right-0 bottom-0 h-full bg-grey w-[300px]"
+            >
+              {/* CLOSE ICON */}
+              <div className="flex justify-end p-12">
+                <button onClick={() => setIsMenuToggled(!isMenuToggled)}>
+                  <img
+                    src={closeButton}
+                    alt="close-icon"
+                  />
+                </button>
+              </div>
+
+              {/* MENU ITEMS */}
+              <motion.div
+                variants={{
+                  hide: {
+                    y: "25%",
+                    opacity: 0,
+                  },
+                  show: {
+                    y: "0%",
+                    opacity: 1,
+                  },
+                }}
+                className="flex flex-col gap-10 ml-[15%] text-2xl text-deep-blue"
+              >
+                <Link
+                  page="Home"
+                  selectedPage={selectedPage}
+                  setSelectedPage={setSelectedPage}
+                />
+                <Link
+                  page="Skills"
+                  selectedPage={selectedPage}
+                  setSelectedPage={setSelectedPage}
+                />
+                <Link
+                  page="Projects"
+                  selectedPage={selectedPage}
+                  setSelectedPage={setSelectedPage}
+                />
+                <Link
+                  page="Contact"
+                  selectedPage={selectedPage}
+                  setSelectedPage={setSelectedPage}
+                />
+              </motion.div>
+            </motion.div>
+          )}
+        </AnimatePresence>
       </div>
     </nav>
   );
